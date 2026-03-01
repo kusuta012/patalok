f9e0dd4b5f7c041c4fe16a60290947f6c0ce1b12
kusuta012 <ishaansardesai315@gmail.com>
2026-03-01 13:56:15 +0530

    Added player mechanics

---
+++ scripts/Player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+@export var speed := 300.0
+@export var jump_velocity := -500.0
+@export var gravity := 1200.0
+
+func _physics_process(delta):
+
+	# Apply gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Jump
+	if Input.is_action_just_pressed("move_up") and is_on_floor():
+		velocity.y = jump_velocity
+
+	# Left / Right movement
+	var direction = Input.get_axis("move_left", "move_right")
+	velocity.x = direction * speed
+
+	move_and_slide()
