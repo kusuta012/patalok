44529d922ddb71bbe9f55ea33ce0653a0bf76c83
kusuta012 <ishaansardesai315@gmail.com>
2026-03-01 17:24:56 +0530

    Added the demon animation

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,21 +1,30 @@
 extends CharacterBody2D
 
-@export var speed := 300.0
-@export var jump_velocity := -300.0
-@export var gravity := 1200.0
+@export var speed : float = 300.0
+
+const GRAVITY = 980.0
+
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
-
 	# Apply gravity
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += GRAVITY * delta
 
-	# Jump
+	# Handle jump
 	if Input.is_action_just_pressed("move_up") and is_on_floor():
-		velocity.y = jump_velocity
+		velocity.y = -500.0
 
-	# Left / Right movement
+	# Handle attack
+	if Input.is_action_just_pressed("attack"):
+		animated_sprite.play("attack")
+
+	# Get the horizontal input direction
 	var direction = Input.get_axis("move_left", "move_right")
-	velocity.x = direction * speed
+	
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
